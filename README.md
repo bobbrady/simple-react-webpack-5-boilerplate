00d44a938f88baf7e6be029c0c245dcc36677858
Bob Brady <bob@digibrady.com>
2020-10-22 17:28:28 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simple React v17 Webpack v5 Boilerplate
+
+A simple bare-bones React v17 Webpack v5 skeleton project with  the following features:
+* Transpiles JS to ES5 with Babel
+* Bundles JS and CSS files
+* Generates an HTML5 file that includes the bundled JS and CSS
+* Basic start command in package.json for webpack dev server hot reloads
+* Basic build command in package.json
+* Follows Facebook create-react-app project structure
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "build"),
+    compress: true,
+    port: 3000,
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+module.exports = config;
